fa322671be72c2043fd0b0dc24f5da816e6ca52e
June Skálletty <101154164+juneskalletty@users.noreply.github.com>
2024-06-25 20:54:26 -0300

    App.jsx
    App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import NavBar from './Components/Navbar.jsx'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />  
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
