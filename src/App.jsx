baf6da9f6a2981cb0fdf50f8d22f2f60d913b322
June Skálletty <101154164+juneskalletty@users.noreply.github.com>
2024-06-25 21:22:06 -0300

    ajustes
    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
-import { navabar } from "./Components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; 
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Titulo from './Components/Titulo';
 
-<navabar/>
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> 
+      <div className="container"> 
+        <Titulo>Componente Home</Titulo> 
+        <Home /> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
